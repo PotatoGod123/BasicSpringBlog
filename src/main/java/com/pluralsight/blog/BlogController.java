c3eceb8fe9f2a579b3c2d1653d5d793f023cb483
Robles <cristian.robles@capgemini.com>
2021-07-26 15:53:08 -0500

    Added code to make all back end functional and updated thymeleaf templating for pages to appear properly

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,8 +1,34 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class BlogController {
 
+    private PostRepository postRepository;
+
+    @RequestMapping("/")
+    public String listPosts(ModelMap map){
+        List<Post> allPost = postRepository.getAllPosts();
+        map.put("posts",allPost);
+        return "home";
+    }
+
+    @RequestMapping("/post/{id}")
+    public String postDetails(@PathVariable Long id, ModelMap map){
+        Post post = postRepository.findById(id);
+        map.put("post",post);
+        return "post-details";
+    }
+
+    public BlogController(PostRepository postRepository){
+        this.postRepository = postRepository;
+    }
 }
